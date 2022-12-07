04c6b6dac9ff1f34f2d024c1ad57d4d57d2f7967
triple <dyd002700@gmail.com>
2022-12-07 19:02:00 +0900

    gradle

---
+++ security/sp-fastcampus-spring-sec/server/basic-test/src/test/java/com/sp/fc/Test1.java
@@ -0,0 +1,13 @@
+package com.sp.fc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class Test1 {
+    @Test
+    void test(){
+        System.out.println("dd");
+    }
+}
