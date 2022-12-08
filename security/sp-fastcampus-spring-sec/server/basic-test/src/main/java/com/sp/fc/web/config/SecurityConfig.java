263e57c701acebf06288060f18a574ffdfcece12
triple <dyd002700@gmail.com>
2022-12-08 18:59:37 +0900

    security-login

--- security/sp-fastcampus-spring-sec/server/basic-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ security/sp-fastcampus-spring-sec/server/basic-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,41 +1,50 @@
 package com.sp.fc.web.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@EnableWebSecurity(debug = true)
+
+@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication()
-                .withUser(User.builder()
-                        .username("user2")
-                        .password(passwordEncoder().encode("2222"))
-                        .roles("USER"))
-                .withUser(User.builder()
-                        .username("admin")
-                        .password(passwordEncoder().encode("admin"))
-                        .roles("ADMIN"));
+    @Bean
+    UserDetailsService userService(){
+        final PasswordEncoder pw = passwordEncoder();
+        UserDetails user2 = User.builder()
+                .username("user2")
+                .password(pw.encode("1234"))
+                .roles("USER").build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(pw.encode("1234"))
+                .roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user2, admin);
     }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests((requests) -> requests
-                .antMatchers("/").permitAll()
-                .anyRequest().authenticated());
-        http.formLogin();
-        http.httpBasic();
-    }
+
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin()
+                .and()
+                .authorizeRequests(auth->{
+                    auth.anyRequest().permitAll()
+                            ;
+                })
+                ;
     }
 }
